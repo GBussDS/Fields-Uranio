a21736d8af85d708efd165756837cbe0acdd12bc
Guilherme Buss <guibuss10@gmail.com>
2024-10-31 11:44:47 -0300

    Adicionei uma visualização para cada uma das páginas.

--- site/pages/1_☢️_Reatores_por_Ano.py
+++ site/pages/1_☢️_Reatores_por_Ano.py
@@ -1,21 +1,28 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="Reatores por Ano",
     page_icon="☢️",
 )
 
-# Initialize connection to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Read data from Google Sheets
-df = conn.read(
-    worksheet="Reatores/Ano",
-    ttl="10m",
-    usecols=[0,1,2,3,4,5,6,7,8,9,10],
-)
+@st.cache_data
+def get_reactor_data():
+    df = conn.read(
+        worksheet="Reatores/Ano",
+        ttl="10m",
+        usecols=[0,1,2,3,4,5,6,7,8,9,10],
+        ) 
+    return df
 
+#Body
 st.write("# Reatores por Ano")
 
 st.write(
@@ -24,4 +31,33 @@ st.write(
     de reatores ativos por ano no mundo."""
 )
 
-st.write(df)
+try:
+    #FIltrando os dados por ano:
+    df = get_reactor_data()
+    years = st.slider(
+        "Escolha os anos", df['Year'].min(), df['Year'].max(), (df['Year'].min(),df['Year'].max())
+    )
+
+    data = df[(df['Year'] >= years[0]) & (df['Year'] <= years[1])]
+
+    #Gráfico
+    st.write("### Energia prodizida por ano")
+
+    fig, ax = plt.subplots()
+    sns.lineplot(data=data, x='Year', y='Electricity Supplied [GW.h]', ax=ax)
+ 
+    st.pyplot(fig)
+
+    #Dados
+    st.write("### Dados usados:")
+
+    st.write(data)
+
+except URLError as e:
+    st.error(
+        """
+        **Erro ao conectar aos dados online.**
+        Connection error: %s
+    """
+        % e.reason
+    )
--- site/pages/2_📋_Informação_dos_Reatores.py
+++ site/pages/2_📋_Informação_dos_Reatores.py
@@ -1,20 +1,30 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import altair as alt
+from urllib.error import URLError
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_page_config(
     page_title="Informações dos Reatores",
     page_icon="📋",
 )
 
-# Initialize connection to Google Sheets
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-# Read data from Google Sheets
-df = conn.read(
-    worksheet="Reatores Info",
-    ttl="10m",
-    usecols=[0,1,2,3,4,5,6,7,8,9,10],
-)
+@st.cache_data
+def get_info_data():
+    df = conn.read(
+        worksheet="Reatores Info",
+        ttl="10m",
+        usecols=[0,1,2,3,4,5,6,7,8,9,10],
+        ) 
+    
+    df['Country'] = df['Country'].astype(str)
+    df['Status'] = df['Status'].astype(str)
+
+    return df
 
 st.write("# Informações dos Reatores")
 
@@ -24,4 +34,37 @@ st.write(
     além de análises sobre modelos/tipos de reatores."""
 )
 
-st.write(df)
+try:
+    #Filtrando os dados por país:
+    df = get_info_data()
+
+    data = df.groupby(['Country', 'Status']).size().reset_index(name='Count')
+    data = data.pivot(index='Country', columns='Status', values='Count').fillna(0)
+
+    operational_counts = df[df['Status'] == 'Operational'].groupby('Country').size()
+    sorted_countries = operational_counts.sort_values(ascending=False).index
+
+    data = data.loc[sorted_countries]
+
+    #Gráfico
+    st.write("### Número de reatores por país:")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    data.plot(kind='bar', stacked=True, ax=ax)
+    ax.set_title("Número de Reatores por Status e País")
+    ax.set_xlabel("País")
+    ax.set_ylabel("Contagem")
+    plt.xticks(rotation=45, ha='right', fontsize=8)
+    st.pyplot(fig)
+
+    #Dados
+    st.write("### Dados usados:", data)
+
+except URLError as e:
+    st.error(
+        """
+        **Erro ao conectar aos dados online.**
+        Connection error: %s
+    """
+        % e.reason
+    )
